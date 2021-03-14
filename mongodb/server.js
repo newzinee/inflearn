be9ebcb586209aa20c928a46711e7a0cbe7baf27
treasureBear94 <qvo7896@gmail.com>
2021-03-14 15:30:19 +0900

    :sparkles: get user, post user

--- mongodb/server.js
+++ mongodb/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get("/", function(req, res) {
-    return res.send('hello world');
+const users = [];
+
+app.get("/user", function(req, res) {
+    return res.send({users: users})
+})
+
+app.post("/user", function(req, res) {
+    users.push({name:"bear", age:30})
+    return res.send({success: true})
 })
 
 app.listen(3000, function() {
