ed56a5a74c99bba384dce0523973633b74363dfd
treasureBear94 <qvo7896@gmail.com>
2021-03-17 23:05:41 +0900

    :sparkles: async

--- mongodb/promise.js
+++ mongodb/promise.js
@@ -8,11 +8,24 @@ const addSum = (a, b) => new Promise((resolve, reject) => {
 })
 
 
-addSum(10,20)
-    .then(sum1 => {
-        console.log({ sum1 })
-        return addSum(sum1, 3)
-    })
-    .then(sum2 => addSum(sum2, 1))
-    .then(sum2 => console.log({ sum2 }))
-    .catch(error => console.log({ error }))
+// addSum(10,20)
+//     .then(sum1 => {
+//         console.log({ sum1 })
+//         return addSum(sum1, 3)
+//     })
+//     .then(sum2 => addSum(sum2, 1))
+//     .then(sum2 => console.log({ sum2 }))
+//     .catch(error => console.log({ error }))
+
+const totalSum = async () => {
+    try {
+        let sum = await addSum(10, 10)
+        let sum2 = await addSum(sum, 10)
+        console.log({ sum, sum2 })
+    } catch(err) {
+        if(err) console.log({ err })
+    }
+    
+}
+
+totalSum();
