3322ca477514daf5ae3089b10677391809e432cb
treasureBear94 <qvo7896@gmail.com>
2021-03-18 23:53:01 +0900

    :sparkles: Member 도메인 생성

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/03/18
+ */
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(final Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(final Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
