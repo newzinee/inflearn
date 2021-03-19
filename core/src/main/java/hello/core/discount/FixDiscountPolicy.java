19cadfb97c18380ea61b2658d7835f7d0b346cd8
treasureBear94 <qvo7896@gmail.com>
2021-03-19 23:46:53 +0900

    :sparkles: 주문, 할인 도메인

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,24 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/03/19
+ */
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(final Member member, final int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+
+}
---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/03/19
+ */
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
+        final Member member = memberRepository.findById(memberId);
+        final int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+}
