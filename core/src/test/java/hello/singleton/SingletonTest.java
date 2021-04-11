ae69e208a1f89f06481d426c59a2c6009c88f62e
treasureBear94 <qvo7896@gmail.com>
2021-04-11 14:55:28 +0900

    :white_check_mark: 싱글톤패턴

--- core/src/test/java/hello/singleton/SingletonTest.java
+++ core/src/test/java/hello/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package hello.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author yjjung
@@ -26,6 +27,18 @@ class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        final SingletonService singletonService1 = SingletonService.getInstance();
+        final SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
