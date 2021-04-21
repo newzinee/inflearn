05a50b3b8cb0870724cb4c6614e4616758d6058d
treasureBear94 <qvo7896@gmail.com>
2021-04-21 18:26:56 +0900

    :sparkles: 빈 생명주기

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,36 @@
+package hello.core.lifecycle;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/04/21
+ */
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + ", message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+
+}
