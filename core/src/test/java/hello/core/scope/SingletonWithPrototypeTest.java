0ce0d2fb7a626f9967044dfc9fa3662d24f809bb
treasureBear94 <qvo7896@gmail.com>
2021-04-24 22:36:01 +0900

    :sparkles: 싱글톤 프로토타입 1

---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -0,0 +1,53 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * @author yjjung
+ * @version 0.1.0
+ * @since 2021/04/24
+ */
+class SingletonWithPrototypeTest {
+
+    @Test
+    void prototypeFind() {
+        final AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        final PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        final PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
