67030d3286b5e918e1cc4a14c51bc713a61b719e
treasureBear94 <qvo7896@gmail.com>
2021-04-25 18:53:12 +0900

    :sparkles: request scope 2

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,9 +14,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(final String id) {
+        final MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 
