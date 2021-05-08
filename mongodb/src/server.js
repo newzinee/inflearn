20c8c3429e05f0cd321450a6c76d2340335c9d63
treasureBear94 <qvo7896@gmail.com>
2021-05-08 20:42:08 +0900

    :recycle: code formatter

--- mongodb/src/server.js
+++ mongodb/src/server.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { userRouter } = require('./routes/userRoute')
+const { userRouter } = require('./routes/userRoute');
 
 const MONGO_URI = 'mongodb+srv://yjjung:@inflean-mongodb.gwxhv.mongodb.net/BlogService?retryWrites=true&w=majority';
 
-const server = async() => {
-    try {
-        await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false });
-        mongoose.set('debug', true)
-        console.log('MongoDB connected')
-        app.use(express.json())
-        
-        app.use('/user', userRouter)
+const server = async () => {
+	try {
+		await mongoose.connect(MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		});
+		mongoose.set('debug', true);
+		console.log('MongoDB connected');
+		app.use(express.json());
 
-        app.listen(3000, () => console.log('server listening on port 3000'))
-    } catch(err) {
-        console.log(err)
-    }
-}
+		app.use('/user', userRouter);
+
+		app.listen(3000, () => console.log('server listening on port 3000'));
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 server();
